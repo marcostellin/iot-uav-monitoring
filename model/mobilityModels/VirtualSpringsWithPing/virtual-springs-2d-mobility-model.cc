09c75cca97f2454bf3cfb35172125c834f5370f5
letal32 <letal32@gmail.com>
2018-05-10 16:03:18 +0100

    Virtual Springs with Ping -beta -not working

---
+++ model/mobilityModels/VirtualSpringsWithPing/virtual-springs-2d-mobility-model.cc
@@ -0,0 +1,430 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2006,2007 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Marco Stellin
+ */
+#include "virtual-springs-2d-mobility-model.h"
+#include "ns3/enum.h"
+#include "ns3/double.h"
+#include "ns3/string.h"
+#include "ns3/pointer.h"
+#include "ns3/simulator.h"
+#include "ns3/log.h"
+#include "ns3/node-list.h"
+#include "ns3/socket.h"
+#include <cmath>
+#include <algorithm>
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("VirtualSprings2d");
+
+NS_OBJECT_ENSURE_REGISTERED (VirtualSprings2dMobilityModel);
+
+TypeId
+VirtualSprings2dMobilityModel::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::VirtualSprings2dMobilityModel")
+    .SetParent<MobilityModel> ()
+    .SetGroupName ("Mobility")
+    .AddConstructor<VirtualSprings2dMobilityModel> ()
+
+    .AddAttribute ("Time",
+                   "Change current direction and speed after moving for this delay.",
+                   TimeValue (Seconds (1.0)),
+                   MakeTimeAccessor (&VirtualSprings2dMobilityModel::m_modeTime),
+                   MakeTimeChecker ())
+
+    .AddAttribute ("Speed",
+                   "The speed of the aerial nodes",
+                   DoubleValue (17.0),
+                   MakeDoubleAccessor (&VirtualSprings2dMobilityModel::m_speed),
+                   MakeDoubleChecker<double> ())
+
+    .AddAttribute ("Tolerance",
+                   "Minimum force to cause displacement",
+                   DoubleValue (30.0),
+                   MakeDoubleAccessor (&VirtualSprings2dMobilityModel::m_tol),
+                   MakeDoubleChecker<double> ())
+                   
+                   
+    .AddAttribute ("kAta",
+                   "Stifness of AtA springs",
+                   DoubleValue (0.5),
+                   MakeDoubleAccessor (&VirtualSprings2dMobilityModel::m_kAta),
+                   MakeDoubleChecker<double> ())
+
+    .AddAttribute ("l0Ata",
+                   "Natural length of AtA springs",
+                   DoubleValue (150),
+                   MakeDoubleAccessor (&VirtualSprings2dMobilityModel::m_l0Ata),
+                   MakeDoubleChecker<double> ())
+
+    .AddAttribute ("l0Atg",
+                   "Natural length of AtG springs",
+                   DoubleValue (20),
+                   MakeDoubleAccessor (&VirtualSprings2dMobilityModel::m_l0Atg),
+                   MakeDoubleChecker<double> ())
+    
+                   
+    .AddAttribute ("TxRangeAta",
+                   "Transmission Range of AtA links",
+                   DoubleValue (300),
+                   MakeDoubleAccessor (&VirtualSprings2dMobilityModel::m_rangeAta),
+                   MakeDoubleChecker<double> ())
+
+    .AddAttribute ("TxRangeAtg",
+                   "Transmission Range of AtG links",
+                   DoubleValue (100),
+                   MakeDoubleAccessor (&VirtualSprings2dMobilityModel::m_rangeAtg),
+                   MakeDoubleChecker<double> ());          
+                   
+                   
+  return tid;
+}
+
+void 
+VirtualSprings2dMobilityModel::ReceivePacket (Ptr<Socket> socket)
+{
+  while (socket->Recv ())
+  {
+      NS_LOG_UNCOND ("Received one packet!");
+      NS_LOG_UNCOND (Simulator::Now().GetSeconds());
+  }
+
+  m_ping = true;
+}
+
+void
+VirtualSprings2dMobilityModel::DoInitialize (void)
+{
+  m_prevVel = Vector(0,0,0);
+  m_socket -> SetRecvCallback (MakeCallback (&ns3::VirtualSprings2dMobilityModel::ReceivePacket, this));
+
+  DoInitializePrivate ();
+  MobilityModel::DoInitialize ();
+}
+
+void
+VirtualSprings2dMobilityModel::DoInitializePrivate (void)
+{
+  m_helper.Update();
+  m_ping = false;
+ 
+  m_socket -> Send (Create<Packet> (56));
+
+  Simulator::Schedule(m_modeTime, &VirtualSprings2dMobilityModel::DoTryMove, this);
+  
+}
+
+void
+VirtualSprings2dMobilityModel::DoTryMove (void)
+{
+  if (m_ping)
+  {
+    double speed = m_speed;
+
+    //Compute the direction of the movement
+    Vector forceAta = VirtualSprings2dMobilityModel::ComputeAtaForce();
+    Vector forceAtg = VirtualSprings2dMobilityModel::ComputeAtgForce();
+    Vector force = forceAta + forceAtg;
+
+    NS_LOG_DEBUG("ForceMod = " << force.GetLength());
+
+    double k = speed/std::sqrt(force.x*force.x + force.y*force.y);
+    Vector vector (k*force.x,
+             k*force.y,
+             0.0);
+
+    NS_LOG_DEBUG("Velocity = " << vector);
+
+    if (force.GetLength () > m_tol)
+    {
+      m_helper.SetVelocity(vector);
+      m_prevVel = vector; //Save velocity vector to go back if needed
+      m_helper.Unpause();
+    }
+    else 
+    {
+      m_helper.Pause();
+    }
+
+    Time delayLeft = m_modeTime;
+
+    DoWalk (delayLeft);
+  }
+  else
+  { 
+    // Vector newVel;
+    // newVel.x = -m_prevVel.x;
+    // newVel.y = -m_prevVel.y;
+    // newVel.z = -m_prevVel.z;
+    // m_helper.SetVelocity(newVel);
+    m_helper.Pause();
+    Time delayLeft = m_modeTime;
+    DoWalk (delayLeft);
+  }
+  
+}
+
+
+Vector
+VirtualSprings2dMobilityModel::ComputeAtaForce()
+{
+  Vector myPos = m_helper.GetCurrentPosition ();
+  //NS_LOG_DEBUG("myPos=" << myPos);
+
+  double fx = 0;
+  double fy = 0;
+  
+  for (uint16_t j = 0; j < m_ataNodes.size(); j++)
+  {
+    Ptr<Node> node = NodeList::GetNode(m_ataNodes[j]);
+    Ptr<MobilityModel> otherMob = node -> GetObject<MobilityModel>();
+    Vector otherPos = otherMob -> GetPosition();
+    double dist = CalculateDistance(myPos, otherPos);
+    //NS_LOG_DEBUG("otherPos=" << otherPos << " distace= " << dist);
+
+    if (dist > 0  && dist < m_rangeAta)
+    {   
+
+        double disp = dist - m_l0Ata;
+        Vector diff = otherPos - myPos;
+        double force = m_kAta*disp;
+        double k = force/std::sqrt(diff.x*diff.x + diff.y*diff.y);
+
+        fx += k*diff.x;
+        fy += k*diff.y;
+
+    }
+   }
+  
+
+  NS_LOG_DEBUG("ATA_FORCE=" << Vector(fx,fy,0));
+
+  return Vector(fx,fy,0);
+}
+
+Vector
+VirtualSprings2dMobilityModel::ComputeAtgForce()
+{
+  Vector myPos = m_helper.GetCurrentPosition ();
+  myPos.z = 0;
+
+  double kAtg = VirtualSprings2dMobilityModel::ComputeKatg();
+
+  double fx = 0;
+  double fy = 0;
+
+  for (uint16_t j = 0; j < m_atgNodes.size(); j++)
+  {
+    Ptr<Node> node = NodeList::GetNode(m_atgNodes[j]);
+    Ptr<MobilityModel> mob = node -> GetObject<MobilityModel>();
+    Vector pos = mob -> GetPosition();
+    pos.z = 0;
+
+    double dist = CalculateDistance(myPos, pos);
+    //NS_LOG_DEBUG("atgNodePos=" << pos << " distace= " << dist);
+
+    if (dist < m_rangeAtg)
+    {   
+
+        double disp = dist - m_l0Atg;
+        Vector diff = pos - myPos;
+        double force = kAtg*disp;
+        double k = force/std::sqrt(diff.x*diff.x + diff.y*diff.y);
+
+        fx += k*diff.x;
+        fy += k*diff.y;
+
+        // double theta = difference.y / difference.x;
+        // fx += -m_kAta*disp*cos(theta);
+    }
+   }
+
+   NS_LOG_DEBUG("ATG_FORCE=" << Vector(fx,fy,0));
+
+   return Vector(fx,fy,0);
+
+}
+
+int
+VirtualSprings2dMobilityModel::GetMaxNodesNeighbours()
+{
+  Vector myPos = m_helper.GetCurrentPosition ();
+  myPos.z = 0;
+
+  int curMax = 0;
+  for (uint16_t j = 0; j < m_ataNodes.size(); j++)
+  {
+    Ptr<Node> ataNode = NodeList::GetNode(m_ataNodes[j]);
+    Ptr<MobilityModel> ataNodeMob = ataNode -> GetObject<MobilityModel>();
+    Vector ataNodePos = ataNodeMob -> GetPosition();
+    ataNodePos.z = 0;
+
+    double dist = CalculateDistance(myPos, ataNodePos);
+
+    if (dist < m_rangeAta)
+    {
+      int numAtgNodes = VirtualSprings2dMobilityModel::ComputeNumGroudNodes(ataNode);
+
+      if (numAtgNodes > curMax){
+        curMax = numAtgNodes;
+      }
+    }
+  }
+
+  //NS_LOG_DEBUG("Max nodes of neighbours = " << curMax);
+
+  return curMax;
+}
+
+double
+VirtualSprings2dMobilityModel::ComputeKatg()
+{
+  int coveredNodes = VirtualSprings2dMobilityModel::ComputeNumGroudNodes();
+  int maxNodesNeighbours = VirtualSprings2dMobilityModel::GetMaxNodesNeighbours();
+
+  if (!maxNodesNeighbours)
+  {
+    //NS_LOG_DEBUG("kAtg = 1" );
+    return 1;
+  }
+
+  //NS_LOG_DEBUG("kAtg = " << coveredNodes/maxNodesNeighbours);
+
+  return coveredNodes/maxNodesNeighbours;
+}
+
+int
+VirtualSprings2dMobilityModel::ComputeNumGroudNodes(Ptr<Node> node )
+{
+  Ptr<MobilityModel> mob = node -> GetObject<MobilityModel>();
+  Vector pos = mob -> GetPosition();
+  pos.z = 0;
+
+  int counter = 0;
+  for (uint16_t j = 0; j < m_atgNodes.size(); j++)
+  {
+    Ptr<Node> atgNode = NodeList::GetNode(m_atgNodes[j]);
+    Ptr<MobilityModel> atgNodeMob = atgNode -> GetObject<MobilityModel>();
+    Vector atgNodePos = atgNodeMob -> GetPosition();
+    atgNodePos.z = 0;
+    double dist = CalculateDistance(pos, atgNodePos);
+
+    if (dist < m_rangeAtg)
+    {
+      counter++;
+    }
+  }
+
+  //NS_LOG_DEBUG("Nodes covered=" << counter);
+
+  return counter;
+}
+
+int
+VirtualSprings2dMobilityModel::ComputeNumGroudNodes()
+{
+  Vector myPos = m_helper.GetCurrentPosition ();
+  myPos.z = 0;
+
+  int counter = 0;
+
+  for (uint16_t j = 0; j < m_atgNodes.size(); j++)
+  {
+    Ptr<Node> node = NodeList::GetNode(m_atgNodes[j]);
+    Ptr<MobilityModel> mob = node -> GetObject<MobilityModel>();
+    Vector pos = mob -> GetPosition();
+    pos.z = 0;
+    double dist = CalculateDistance(myPos, pos);
+
+    if (dist < m_rangeAtg)
+    {
+      counter++;
+    }
+  }
+
+  //NS_LOG_DEBUG("Nodes covered=" << counter);
+
+  return counter;
+
+}
+
+void
+VirtualSprings2dMobilityModel::AddAtaNode(uint32_t id)
+{
+  m_ataNodes.push_back(id); 
+}
+
+
+void
+VirtualSprings2dMobilityModel::AddAtgNode(uint32_t id)
+{
+  m_atgNodes.push_back(id); 
+}
+
+void 
+VirtualSprings2dMobilityModel::AddSocket(Ptr<Socket> socket)
+{
+  m_socket = socket;
+}
+
+
+void
+VirtualSprings2dMobilityModel::DoWalk (Time delayLeft)
+{
+  m_event.Cancel ();
+
+  m_event = Simulator::Schedule (delayLeft, &VirtualSprings2dMobilityModel::DoInitializePrivate, this);
+
+  NotifyCourseChange ();
+}
+
+
+void
+VirtualSprings2dMobilityModel::DoDispose (void)
+{
+  // chain up
+  MobilityModel::DoDispose ();
+}
+Vector
+VirtualSprings2dMobilityModel::DoGetPosition (void) const
+{
+  m_helper.Update();
+  return m_helper.GetCurrentPosition ();	
+
+}
+void
+VirtualSprings2dMobilityModel::DoSetPosition (const Vector &position)
+{ 
+  m_helper.SetPosition (position);
+  Simulator::Remove (m_event);
+  m_event = Simulator::ScheduleNow (&VirtualSprings2dMobilityModel::DoInitializePrivate, this);
+
+}
+Vector
+VirtualSprings2dMobilityModel::DoGetVelocity (void) const
+{
+  
+  return m_helper.GetVelocity ();
+  
+}
+
+
+
+} // namespace ns3
